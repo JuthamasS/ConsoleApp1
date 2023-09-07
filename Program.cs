7fd6f21ed588895ce2beda9295c789baddda75f1
juthamas saikaew <juthamas.s@piesoft.net>
2023-09-07 16:57:28 +0700

    edit again from hotfixFirst

--- Program.cs
+++ Program.cs
@@ -21,6 +21,8 @@
             Console.WriteLine("Edit again and again from branch release 0.1.0 and push.");
             
             Console.WriteLine("Edit from branch hotfixFirst and push.");
+
+            Console.WriteLine("Edit again from branch hotfixFirst and push.");
         }
     }
 }
